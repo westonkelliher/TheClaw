4c7d174575b898dba4821ac0bae4b0c48ad43f84
Weston <westonkelliher@gmail.com>
2024-10-02 18:17:57 -0700

    reorganize and add spherical-ish rock

--- claw_stuff/dual_arm.gd
+++ claw_stuff/dual_arm.gd
@@ -14,19 +14,15 @@ func _physics_process(delta: float) -> void:
 	var base_stick_L := gpi.stick_L
 	var agreement: float = max(0.0, gpi.stick_L.dot(gpi.stick_R))
 	var disagreement: float = min(0.0, gpi.stick_L.dot(gpi.stick_R))
-	print(disagreement)
-	print(agreement)
 	handle_input(gpi.stick_L*(1.0-agreement*0.5), 0.0, delta)
 	#print($Arm/ForeArm.global_position)
 	$Arm/ForeArm.handle_input(gpi.stick_R*(1.0+disagreement*0.2), 0.0, delta)
-	if Input.is_action_just_pressed("RB"):
-		$Arm/ForeArm/MagHand.set_attraction(1.0)
-	if Input.is_action_just_released("RB"):
-		$Arm/ForeArm/MagHand.set_attraction(0.0)
-	if Input.is_action_just_pressed("LB"):
-		$Arm/ForeArm/MagHand.set_attraction(-1.0)
-	if Input.is_action_just_released("LB"):
-		$Arm/ForeArm/MagHand.set_attraction(0.0)
-	$Arm/ForeArm/MagHand.set_attraction(gpi.trig_R)
+	$Arm/ForeArm/MagHand.set_attraction(0)
+	if gpi.trig_R != 0.0:
+		$Arm/ForeArm/MagHand.set_attraction(gpi.trig_R)
 	if gpi.trig_L != 0.0:
-		$Arm/ForeArm/MagHand.set_attraction(-gpi.trig_L)
+		$Arm/ForeArm/MagHand.set_attraction(-gpi.trig_L*0.5)
+	if Input.is_action_pressed("RB"):
+		$Arm/ForeArm/MagHand.set_attraction(4.0)
+	if Input.is_action_pressed("LB"):
+		$Arm/ForeArm/MagHand.set_attraction(-2.0)
